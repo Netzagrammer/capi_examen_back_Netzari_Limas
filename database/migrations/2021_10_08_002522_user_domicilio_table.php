4f362c6351f05a53ec8549990219ccd6004c2d13
Netzari <nlimas@almexref.com>
2021-10-07 19:33:04 -0500

    Creacion de proyecto y se agrega tabla de users_domicilio.

---
+++ database/migrations/2021_10_08_002522_user_domicilio_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserDomicilioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('domicilio');
+            $table->integer('numero_exterior');
+            $table->string('colonia');
+            $table->integer('cp');
+            $table->string('ciudad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+}
